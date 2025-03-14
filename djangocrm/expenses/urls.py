e102108a3eaf9b4e1f7e7c4df41a22092088cfa3
terrytawe <ttawemak@gmail.com>
2025-03-14 17:28:00 +0000

    Added authentication base files

--- djangocrm/expenses/urls.py
+++ djangocrm/expenses/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="expenses"),
-    path('add-expense', views.add_expense, name="add-expense" ) 
+    path('add_expense', views.add_expense, name="add_expense" ) 
 ]
