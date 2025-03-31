c0de6300c63f0796ceef6ddf94a3472003161418
terrytawe <ttawemak@gmail.com>
2025-03-31 17:51:50 +0000

    Added side panel for login

--- djangocrm/authentication/urls.py
+++ djangocrm/authentication/urls.py
@@ -1,4 +1,5 @@
-from .views import RegistrationView, LoginView,PasswordResetView,PasswordNewView,UsernameValidationView, EmailValidationView, ActivationView
+from .views import RegistrationView, LoginView,PasswordResetView,PasswordNewView,UsernameValidationView
+from .views import EmailValidationView, ActivationView, LogoutView
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 
@@ -9,5 +10,6 @@ urlpatterns = [
     path('password-reset', PasswordResetView.as_view(), name="password-register"),
     path('username-validation', csrf_exempt(UsernameValidationView.as_view()), name="username-validation"),
     path('email-validation', csrf_exempt(EmailValidationView.as_view()), name="email-validation"),
-    path('activate/<uidb64>/<token>', ActivationView.as_view(), name='activate' )
+    path('activate/<uidb64>/<token>', ActivationView.as_view(), name='activate' ),
+    path('logout', LogoutView.as_view(), name='logout' )
 ]
--- djangocrm/authentication/views.py
+++ djangocrm/authentication/views.py
@@ -5,13 +5,15 @@ from django.http import JsonResponse
 from django.contrib.auth.models import User
 from validate_email import validate_email # type: ignore
 from django.contrib import messages, auth
-from django.core.mail import EmailMessage, send_mail
+from django.core.mail import EmailMessage, EmailMultiAlternatives, send_mail
 from django.conf import settings
 from django.urls import reverse
-from django.utils.encoding import force_bytes, DjangoUnicodeDecodeError
+from django.utils.encoding import force_bytes, force_str, DjangoUnicodeDecodeError
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.contrib.sites.shortcuts import get_current_site
-from .utils import TokenGenerator
+from django.template.loader import render_to_string
+from .utils import activation_token
+from django.utils.html import strip_tags
 
 
 
@@ -78,32 +80,32 @@ class RegistrationView(View):
                 user.save()
 
                 uidb64 = urlsafe_base64_encode(force_bytes(user.pk))
-                link = reverse('activate', kwargs={'uidb64': uidb64, 'token': TokenGenerator.make_token(user)})
+                link = reverse('activate', kwargs={'uidb64': uidb64, 'token': activation_token.make_token(user)})
                 activate_url = 'http://' + get_current_site(request).domain + link
 
                 email_subject = 'Activate your account'
-                email_body = f"""
-                                    Hi {user.username}, Your Django CRM account 
-                                    has been created successfully.  
-                                                                       
-                                    Proceed to activate your account using the link below: 
-                                    {activate_url}
-
-                                    """
+                # email_body = render_to_string('authentication/activation.html', {'username': username, 'link': activate_url})
                 email_sender = 'noreply.developer00@gmail.com'
                 email_recipient = [email,]
 
-                send_mail(
-                    email_subject,
-                    email_body,
-                    email_sender,
-                    email_recipient,
-                    fail_silently=False,
-                )
+                html_content = render_to_string("authentication/activation.html", {
+                    'username': username, 
+                    'link': activate_url
+                })
 
-                message_success = 'Account successfully created. click <a href="' + activate_url +'">here</a> to login'
-                         
-                messages.success(request, message_success, extra_tags='safe')
+                email_body = strip_tags(html_content)
+                email = EmailMultiAlternatives(
+                    email_subject, 
+                    email_body, 
+                    email_sender, 
+                    email_recipient
+                )
+                email.attach_alternative(html_content, "text/html")
+                email.send(fail_silently=False)
+
+                message_success = 'Your account has been successfully created. Please check your email and confirm'
+                messages.success(request, message_success)
+
                 return render(request, 'authentication/register.html')
 
         return render(request, 'authentication/register.html')
@@ -112,7 +114,25 @@ class RegistrationView(View):
 #View to render login page
 #--------------------------------------------------------------------------------------------------    
 class ActivationView(View):
-    def get(self, request, uidb64):
+    def get(self, request, uidb64, token):
+        try:
+            id = force_str(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(pk=id)
+
+            if not activation_token.check_token(user, token):
+                return redirect('login'+'?message='+'User already activated')
+
+            if user.is_active:
+                return redirect('login')
+            user.is_active = True
+            user.save()
+
+            messages.success(request, 'Account activated successfully')
+            return redirect('login')
+
+        except Exception as ex:
+            pass
+
         return redirect('login')
 
 
@@ -122,6 +142,30 @@ class ActivationView(View):
 #--------------------------------------------------------------------------------------------------    
 class LoginView(View):
     def get(self, request):
+        return render(request, 'authentication/login.html')
+    
+    def post(self, request):
+        username = request.POST['username']
+        password = request.POST['password']
+
+        if username and password:
+            user = auth.authenticate(username=username, password=password)
+
+            if user:
+                if user.is_active:
+                    auth.login(request, user)
+                    messages.success(request, 'Welcome, ' +
+                                     user.username+' you are now logged in')
+                    return redirect('expenses')
+                messages.error(
+                    request, 'Account is not active,please check your email')
+                return render(request, 'authentication/login.html')
+            messages.error(
+                request, 'Invalid credentials,try again')
+            return render(request, 'authentication/login.html')
+
+        messages.error(
+            request, 'Please fill all fields')
         return render(request, 'authentication/login.html')
 
 #--------------------------------------------------------------------------------------------------    
@@ -137,4 +181,19 @@ class PasswordResetView(View):
 class PasswordNewView(View):
     def get(self, request):
         return render(request, 'authentication/password-new.html')
+
+
+#--------------------------------------------------------------------------------------------------    
+#View to process logout action
+#--------------------------------------------------------------------------------------------------    
+class LogoutView(View):
+    def get(self, request):
+        auth.logout(request)
+
+        # Clear any existing messages
+        storage = messages.get_messages(request)
+        list(storage)  # This consumes the generator and clears it
+
+        messages.success(request, 'You have been logged out')
+        return redirect('login')
    
